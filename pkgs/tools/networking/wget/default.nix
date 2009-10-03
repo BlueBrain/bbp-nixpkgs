cb0d78d7e7bdc0a53e578e93bf03797c207adff5
Ludovic Courtès <ludo@gnu.org>
2009-10-03 13:42:01 +0000

    GNU Wget 1.12.
    svn path=/nixpkgs/branches/stdenv-updates/; revision=17629

--- pkgs/tools/networking/wget/default.nix
+++ pkgs/tools/networking/wget/default.nix
@@ -1,17 +1,31 @@
 {stdenv, fetchurl, gettext, openssl ? null}:
 
 stdenv.mkDerivation rec {
-  name = "wget-1.11.4";
-  
+  name = "wget-1.12";
+
   src = fetchurl {
     url = "mirror://gnu/wget/${name}.tar.bz2";
-    sha256 = "1yr7w182n7lvkajvq07wnw65sw2vmxjkc3611kpc728vhvi54zwb";
+    sha256 = "16msgly5xn0qj6ngsw34q9j7ag8jkci6020w21d30jgqw8wdj8y8";
   };
 
   buildInputs = [gettext openssl];
 
+  doCheck = true;
+
   meta = {
-    description = "A console downloading program. Has some features for mirroring sites.";
-    homepage = http://www.gnu.org/software/wget;
+    description = "GNU Wget, a tool for retrieving files using HTTP, HTTPS, and FTP";
+
+    longDescription =
+      '' GNU Wget is a free software package for retrieving files using HTTP,
+         HTTPS and FTP, the most widely-used Internet protocols.  It is a
+         non-interactive commandline tool, so it may easily be called from
+         scripts, cron jobs, terminals without X-Windows support, etc.
+      '';
+
+    license = "GPLv3+";
+
+    homepage = http://www.gnu.org/software/wget/;
+
+    maintainers = [ stdenv.lib.maintainers.ludo ];
   };
 }
