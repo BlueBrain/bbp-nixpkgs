33c4e733993ab2ffabef1ede6cf9cfdb919fbb5b
Yury G. Kudryashov <urkud.urkud@gmail.com>
2010-05-01 16:12:32 +0000

    pkgs/development/libraries/mesa/default.nix: drop trailing white space
    From trunk@21428
    
    svn path=/nixpkgs/branches/mass-update-01/; revision=21510

--- pkgs/development/libraries/mesa/default.nix
+++ pkgs/development/libraries/mesa/default.nix
@@ -6,22 +6,22 @@ else
 
 stdenv.mkDerivation {
   name = "mesa-7.8.1";
-  
+
   src = fetchurl {
     url = ftp://ftp.freedesktop.org/pub/mesa/7.8.1/MesaLib-7.8.1.tar.bz2;
     sha256 = "1yh717x4qxmild1s15qyv68irkrbm5gi4273052v8pfppxd6xd5h";
   };
 
   configureFlags = "--disable-gallium";
-  
+
   buildInputs =
     [ pkgconfig expat x11 libdrm xlibs.glproto
       xlibs.libXxf86vm xlibs.libXfixes xlibs.libXdamage xlibs.dri2proto
       lipo
     ];
-  
+
   passthru = { inherit libdrm; };
-  
+
   meta = {
     description = "An open source implementation of OpenGL";
     homepage = http://www.mesa3d.org/;
