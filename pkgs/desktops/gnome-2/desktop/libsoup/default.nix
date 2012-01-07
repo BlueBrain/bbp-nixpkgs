4cd8013a8f1f953378d015e8f0ce8f117511ee21
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-07 19:43:25 +0000

    libsoup-2.36.1
    svn path=/nixpkgs/branches/glib-2.30/; revision=31419

--- pkgs/desktops/gnome-2/desktop/libsoup/default.nix
+++ pkgs/desktops/gnome-2/desktop/libsoup/default.nix
@@ -1,12 +1,24 @@
-{stdenv, fetchurl, pkgconfig, libxml2, gnutls, libproxy, sqlite, curl, 
-  glib, GConf, gnome_keyring}:
+{ stdenv, fetchurl, glib, libxml2, pkgconfig, xz
+, gnomeSupport ? true, libgnome_keyring, sqlite, glib_networking }:
 
-stdenv.mkDerivation rec {
-  name = "libsoup-2.33.6";
+stdenv.mkDerivation {
+  name = "libsoup-2.36.1";
+
   src = fetchurl {
-    url = "mirror://gnome/sources/libsoup/2.33/${name}.tar.bz2";
-    sha256 = "988f7897fe125a77a5946b2fd6d47d7374fd94a1406e810482cfff6a52a6a923";
+    url = mirror://gnome/sources/libsoup/2.36/libsoup-2.36.1.tar.xz;
+    sha256 = "0r8zkr0a328jkww4dv9z1q691rw59nh4lf5f5pzzr9szzw3j8wkk";
   };
-  buildInputs = [ pkgconfig libxml2 gnutls libproxy sqlite curl 
-    glib GConf gnome_keyring ];
+
+
+  buildNativeInputs = [ pkgconfig xz ];
+  propagatedBuildInputs = [ glib libxml2 ]
+    ++ stdenv.lib.optionals gnomeSupport [ libgnome_keyring sqlite ];
+  passthru.propagatedUserEnvPackages = [ glib_networking ];
+
+  # glib_networking is a runtime dependency, not a compile-time dependency
+  configureFlags = "--disable-tls-check";
+
+  meta = {
+    inherit (glib.meta) maintainers platforms;
+  };
 }
