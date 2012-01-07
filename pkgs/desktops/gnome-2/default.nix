99be946dba8778af30f9bffbdf65fd88522bb218
Yury G. Kudryashov <urkud.urkud@gmail.com>
2012-01-07 19:43:07 +0000

    Add gnome2.libgnome_keyring
    svn path=/nixpkgs/branches/glib-2.30/; revision=31416

--- pkgs/desktops/gnome-2/default.nix
+++ pkgs/desktops/gnome-2/default.nix
@@ -52,6 +52,8 @@
 
   gnome_vfs_monikers = callPackage ./platform/gnome-vfs-monikers { };
 
+  libgnome_keyring = callPackage ./platform/libgnome-keyring { };
+
   libgnome = callPackage ./platform/libgnome { };
 
   libgnomeui = callPackage ./platform/libgnomeui { };
