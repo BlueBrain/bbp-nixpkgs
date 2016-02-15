f50578fee7a1ec8d42de4c45904f94e59a3411e7
Adrien Devresse <adrien.devresse@epfl.ch>
2016-02-15 14:58:34 +0000

    Ass an automatic switch to tarball download if submodule is not initialized

--- default.nix
+++ default.nix
@@ -9,7 +9,9 @@
 # import std package from Nix Upstream
 
 let 
-	std-pkgs = args: (import ./std-nixpkgs args );
+	std-pkgs = args: (if builtins.pathExists ./std-nixpkgs/default.nix
+				then (import ./std-nixpkgs args) 
+				else (import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/9fe0c23a23507d166d23dede5c3176eadd89e2b6.tar.gz) args));
 	patch-pkgs = args: (import ./patch-nixpkgs { std-pkgs = std-pkgs args; } );
 	bbp-pkgs = args: (import ./bbp-nixpkgs/all.nix { std-pkgs = patch-pkgs args; } );
 in
