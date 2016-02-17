a5a89856dbb7386bd0a963bfe7cd393419b13f4c
Adrien Devresse <adrien.devresse@epfl.ch>
2016-02-17 14:24:06 +0100

    Update weblink dependency

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@
 let 
 	std-pkgs = args: (if builtins.pathExists ./std-nixpkgs/default.nix
 				then (import ./std-nixpkgs args) 
-				else (import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/9fe0c23a23507d166d23dede5c3176eadd89e2b6.tar.gz) args));
+				else (import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/9220f03d08e4ba914605256aa22ef2a67a9a21ac.tar.gz) args));
 	patch-pkgs = args: (import ./patch-nixpkgs { std-pkgs = std-pkgs args; } );
 	bbp-pkgs = args: (import ./bbp-nixpkgs/all.nix { std-pkgs = patch-pkgs args; } );
 in
