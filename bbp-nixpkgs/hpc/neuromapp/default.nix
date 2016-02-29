fdae098abbee2f6990607abe85632db88765e04e
Adrien Devresse <adrien.devresse@epfl.ch>
2016-02-29 18:33:46 +0100

    Add steps patch, add neuromapp modification

--- bbp-nixpkgs/hpc/neuromapp/default.nix
+++ bbp-nixpkgs/hpc/neuromapp/default.nix
@@ -1,14 +1,22 @@
-{ stdenv, fetchFromGitHub, pkgconfig, boost, cmake, mpiRuntime, doxygen }:
+{ stdenv
+, fetchFromGitHub
+, pkgconfig
+, boost
+, cmake
+, mpiRuntime
+, ncurses
+, readline
+, doxygen }:
 
 stdenv.mkDerivation rec {
   name = "neuromapp-1.0.0";
-  buildInputs = [ stdenv pkgconfig boost cmake mpiRuntime doxygen];
+  buildInputs = [ stdenv pkgconfig boost cmake mpiRuntime doxygen readline ncurses ];
 
   src = fetchFromGitHub {
     owner = "BlueBrain";
     repo = "neuromapp";
-    rev = "4ed218a746cd0e4c5d1c193fac30942707b3b521";
-    sha256 = "1rmndp5gjx4cm51sklw3zd5nrjyd84y5kq45x808r985accyj1nw";
+    rev = "c65d920e206311709ef900aabb6c2690f602a6f8";
+    sha256 = "0hgjj39wgdzvn6rsf6wmj46fwynfmjyrm2fzk918svvpc4sa70ya";
   };
     
   cmakeFlags= "-DBoost_NO_BOOST_CMAKE=TRUE -DBoost_USE_STATIC_LIBS=FALSE ";
