e0c2bff45d9b9ec721e8585b2d6cff94b93c3cba
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-03 00:17:46 +0100

    Switch coreneuron to the new deterministic version of git external

--- bbp-nixpkgs/hpc/coreneuron/default.nix
+++ bbp-nixpkgs/hpc/coreneuron/default.nix
@@ -1,15 +1,14 @@
-{ stdenv, fetchgitPrivate, perl, cmake, boost, cmake-external, pkgconfig, mpiRuntime, mod2c, mods-src}:
+{ stdenv, fetchgitExternal, perl, cmake, boost, pkgconfig, mpiRuntime, mod2c, mods-src}:
 
 stdenv.mkDerivation rec {
   name = "coreneuron-0.7.0";
-  buildInputs = [ stdenv perl cmake cmake-external boost pkgconfig mpiRuntime mod2c];
+  buildInputs = [ stdenv perl cmake boost pkgconfig mpiRuntime mod2c];
 
 
-  src = fetchgitPrivate {
+  src = fetchgitExternal {
     url = "ssh://bbpcode.epfl.ch/sim/coreneuron";
     rev =  "a7231d5aa7725b87c772b690c0f96b0404319414";
-    sha256 = "0rlg4dj1rlc546xwlvvqak5nl8vg0217bwh1rm550qx7v3n6svp3";
-    leaveDotGit = true;    
+    sha256 = "0618w46ximr1cp02w19fdvqnqlm37456f8lg3iqik7lridzxck2b";   
   };
 
 
