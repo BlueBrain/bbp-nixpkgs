5b58778d64bc9f6e56c8cd504635b9a2625f5d5c
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-02 20:49:57 +0100

    Update shasum on cyme, leaveDotGit understiministic problem

--- bbp-nixpkgs/hpc/cyme/default.nix
+++ bbp-nixpkgs/hpc/cyme/default.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = "https://github.com/BlueBrain/cyme.git";
     rev = "f8c3708ae10a85706ac487400da75e8e03c0b5b9";
-    sha256 = "1yv8cj782y9nb8r2ks8msdqywvkll88hhf3zzf5q4li459g617zh";
+    sha256 = "0fbl83zlbnhmkzmn4s12rncj9l17qcwirg0d2xkqw1qgyagg0ibd";
     leaveDotGit = true;
   }; 
 
