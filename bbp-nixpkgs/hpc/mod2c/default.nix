3ede458e204ea638f1dc7f9cc4a96dc1958f98f9
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-02 23:37:32 +0100

    Switch mod2 to deterministic new git external

--- bbp-nixpkgs/hpc/mod2c/default.nix
+++ bbp-nixpkgs/hpc/mod2c/default.nix
@@ -1,15 +1,14 @@
-{ stdenv, fetchgitPrivate, cmake, cmake-external, pkgconfig, bison, flex, python }:
+{ stdenv, fetchgitExternal, cmake, pkgconfig, bison, flex, python }:
 
 stdenv.mkDerivation rec {
   name = "mod2c-1.1.1";
-  buildInputs = [ stdenv cmake cmake-external pkgconfig bison flex python];
+  buildInputs = [ stdenv cmake pkgconfig bison flex python];
 
 
-  src = fetchgitPrivate {
+  src = fetchgitExternal {
     url = "ssh://bbpcode.epfl.ch/sim/mod2c";
     rev = "b3bb86294e1fea3d0768291bec9f062db78bc8db";
-    sha256 = "1rihcss143fsjv9l7bmxk8swk8w711dykamlab8kg5hzww5rmy06";
-    leaveDotGit = true;
+    sha256 = "0wwjw9225w4cf0mx3z5042y6zjbqimq724hl0pd300bqqlfrsvff";
   };
    
   
