c0067508cff5774bfe1112b71c0daa7e876eaa59
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-02 11:28:37 +0100

    Switch vmmlib to leaveDotGit instead of deepClone Update to new hash

--- bbp-nixpkgs/common/vmmlib/default.nix
+++ bbp-nixpkgs/common/vmmlib/default.nix
@@ -7,8 +7,8 @@ stdenv.mkDerivation rec {
   src = fetchgit {
     url = "https://github.com/Eyescale/vmmlib.git";
     rev= "b41e1a5574bf6405a5076d4e5c9c27c73dea96d3";
-    sha256 = "0q8w6kg6kgf1g4b2i1cbph1f5l0vyjnz62y3id1i9ja6afahyi1n";
-    deepClone = true;
+    sha256 = "1gxydlj5a5ajkgwij7qh4hk7fw940524mkd699vwxkmfibaj9nyx";
+    leaveDotGit = true;
   };
   
   
