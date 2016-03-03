14b7f215f74e0cf4e9916b099409d090dcdc4dbc
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-03 10:35:03 +0100

    Switch servus to the new  deterministic git external support

--- bbp-nixpkgs/viz/servus/default.nix
+++ bbp-nixpkgs/viz/servus/default.nix
@@ -1,15 +1,13 @@
-{ stdenv, fetchgit, cmake, cmake-external, boost, pkgconfig, doxygen }:
+{ stdenv, fetchgitExternal, cmake,  boost, pkgconfig, doxygen }:
 
 stdenv.mkDerivation rec {
   name = "servus-1.1.0-stable";
-  buildInputs = [ stdenv pkgconfig boost cmake cmake-external doxygen];
+  buildInputs = [ stdenv pkgconfig boost cmake doxygen];
 
-  src = fetchgit {
-    url = "https://github.com/HBPVIS/Servus.git";
-    
+  src = fetchgitExternal {
+    url = "https://github.com/HBPVIS/Servus.git";  
     rev = "0a5c5e4aeb6479e4ed6346b0aa9197a14d38afa4";
-    sha256 = "1rpx3jcjmw4gw9f0iiwshdvk0qjbfmxifi20klrjim5xhys64xrn";
-    deepClone = true;
+    sha256 = "000whyg90wcgzhlrrg1b81ln8kjg25z56qm0jsaiab0qir3dgb6q";
   };
   
 
