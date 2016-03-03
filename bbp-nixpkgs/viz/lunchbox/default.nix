51f97d1cebcfcdfdfeaca72a3f06ec70f5c58626
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-03 10:36:08 +0100

    Switch lunchbox to the new  deterministic git external support

--- bbp-nixpkgs/viz/lunchbox/default.nix
+++ bbp-nixpkgs/viz/lunchbox/default.nix
@@ -1,14 +1,13 @@
-{ stdenv, boost, fetchgit, cmake, cmake-external, servus, pkgconfig, leveldb, doxygen }:
+{ stdenv, boost, fetchgitExternal, cmake, servus, pkgconfig, leveldb, doxygen }:
 
 stdenv.mkDerivation rec {
   name = "lunchbox-1.12.0";
-  buildInputs = [ stdenv boost pkgconfig cmake-external servus cmake leveldb doxygen];
+  buildInputs = [ stdenv boost pkgconfig servus cmake leveldb doxygen];
 
-  src = fetchgit{
+  src = fetchgitExternal{
     url = "https://github.com/Eyescale/Lunchbox.git";
     rev = "01caa155d89e9f56318319fd236be1b0bfc1ed03";
-    sha256 = "0n4jwkjlmz7hy6xb3jhrca2vm7hrpsbxpfqxlvrkm2hdw2x0a3rz";
-    deepClone = true;
+    sha256 = "1hh58n3bn1qilmlwd74j5lx1kycd4j772g5slckf8aiy123ia6ya";
   };
   
   enableParallelBuilding = true;
