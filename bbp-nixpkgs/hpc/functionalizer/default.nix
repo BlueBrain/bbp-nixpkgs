bef902f6c97951ed51e873e44ef3fb1d0cb81ae0
Adrien Devresse <adrien.devresse@epfl.ch>
2016-03-21 18:12:30 +0100

    Update circuit-building to trunk

--- bbp-nixpkgs/hpc/functionalizer/default.nix
+++ bbp-nixpkgs/hpc/functionalizer/default.nix
@@ -6,8 +6,8 @@ stdenv.mkDerivation rec {
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/building/Functionalizer";
-    rev = "182addb2847d4fc401df0e13f81fdbf32c8f1ac1";
-    sha256 = "14ffyqxb9q1hy532ibssw2sxmkqc57j8v71l62bf85gd50yiz48f";
+    rev = "388fb479fa261231d0ecde9b64020e5e1027dea8";
+    sha256 = "0bzq3097j56sa3x5xnbi3a4nrflyh0nqrb3877qc91zlnsvsp7ld";
   };
   
   cmakeFlags="-DBoost_USE_STATIC_LIBS=FALSE -DUNIT_TESTS=TRUE";   
