d43a8368421b2e4a0ab080d7e3f9afd7ed91f5b1
Adrien Devresse <adrien.devresse@epfl.ch>
2016-04-13 10:36:06 +0200

    Update touchDetector with doc support

--- bbp-nixpkgs/hpc/touchdetector/default.nix
+++ bbp-nixpkgs/hpc/touchdetector/default.nix
@@ -1,26 +1,43 @@
-{ stdenv, 
-fetchgitPrivate, 
-pkgconfig, 
-boost, 
-hpctools, 
-libxml2, 
-cmake, 
-mpiRuntime, 
-zlib, 
-hdf5
+{ 
+  stdenv
+, fetchgitPrivate
+, pkgconfig
+, boost
+, hpctools
+, libxml2 
+, cmake
+, mpiRuntime
+, zlib 
+, hdf5
+, generateDoc ? true
+, asciidoc
+, xmlto
+, docbook_xsl
+, libxslt 
 }:
 
 stdenv.mkDerivation rec {
   name = "touchdetector-4.3.0-DEV";
-  buildInputs = [ stdenv pkgconfig boost hpctools zlib cmake mpiRuntime libxml2  hdf5 ];
+  buildInputs = [ stdenv pkgconfig boost hpctools zlib cmake mpiRuntime libxml2  hdf5 ]
+	++ stdenv.lib.optional (generateDoc == true) [ asciidoc xmlto docbook_xsl libxslt ];
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/building/TouchDetector";
-    rev = "8e81baec1380e8ee2041c046c80d40549c5514cd";
-    sha256 = "04hkbp5vng6s5g1lkp3dxzl628ycf4a4m8x3w33yjbkb1j105i64";
+    rev = "a924602d90db567ac283abdd42fa25487ecf990a";
+    sha256 = "0xcjh299xhmmvv42pq91k6v0xq677b1ax9xb7j60raa3mm2r39ld";
   };
 
+  cmakeFlags =  stdenv.lib.optional (generateDoc == true) [ "-DTOUCHDETECTOR_DOCUMENTATION=TRUE" ]; 
+
+  postInstall = ''
+		install -D ../LICENSE.txt $out/share/doc/TouchDetector/LICENSE.txt ;
+		install -D ../README.txt $out/share/doc/TouchDetector/README.txt ;
+		'';
+
   enableParallelBuilding = true;
+
+  outputs =  [ "out" "doc" ];
+
 }
 
 
