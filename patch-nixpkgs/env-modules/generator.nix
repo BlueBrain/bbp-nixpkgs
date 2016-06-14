431b239f8145102050695a80f2e70fe3a1627a31
Adrien Devresse <adrien.devresse@epfl.ch>
2016-06-14 20:07:19 +0200

    Enhance modules generation Prepare architecture to generate generic modules for viz cluster

--- patch-nixpkgs/env-modules/generator.nix
+++ patch-nixpkgs/env-modules/generator.nix
@@ -2,8 +2,11 @@
 stdenv,
 buildEnv,
 name,
-moduleFilePath ? name,
+version ? "",
+moduleFilePath ? "",
+conflicts ? [] ,
 packages,
+isLibrary ? false,
 prefixDir ? "",
 description ? "" ,
 extraContent ? ""
@@ -13,60 +16,88 @@ assert builtins.isList packages;
 assert builtins.length packages > 0;
 
  let
-	pathSuffixExist = suffix:
-			let 
-				subPathExist = prefix: builtins.pathExists (prefix + suffix);
-			in 
-				builtins.any subPathExist  packages;
+    pathSuffixExist = suffix:
+            let 
+                subPathExist = prefix: builtins.pathExists (prefix + suffix);
+            in 
+                builtins.any subPathExist  packages;
+    
+    moduleFileSuffix = (if moduleFilePath == "" then
+                        (if version != ""
+                            then "${name}/${version}" 
+                            else "${name}")
+                        else moduleFilePath);
+                        
+     depBuilder = depPrefixString: depList:  if depList == [] 
+                                                then ''''
+                                                else ''
+                                                ${depPrefixString} ${(builtins.head (depList)).modulename}
+                                                ${depBuilder depPrefixString (builtins.tail depList)}
+                                                     '';
+            
+                            
  in
 stdenv.mkDerivation rec {
 
-	inherit name;
+    inherit name;
+    inherit version;
 
-	unpackPhase = ''echo "no sources needed"'';
-	
-	targetEnv =  buildEnv {
-		name = "generated-env-module-${name}";
-		paths = packages;
-	};
-	
-	
-	targetEnvBin = "${targetEnv}/bin";
-	targetEnvPkgConfig = "${targetEnv}/lib/pkgconfig";
-	targetEnvMan = "${targetEnv}/share/man";
-	targetEnvPython = "${targetEnv}/lib/python2.7";
-	targetEnvHoc = "${targetEnv}/hoc";
-	targetModlUnit = "${targetEnv}/share/nrnunits.lib";
+    unpackPhase = ''echo "no sources needed"'';
+    
+    targetEnv =  buildEnv {
+        name = "generated-env-module-${name}";
+        paths = packages;
+    };
+    
+    
+    targetEnvBin = "${targetEnv}/bin";
+    targetEnvPkgConfig = "${targetEnv}/lib/pkgconfig";
+    targetEnvMan = "${targetEnv}/share/man";
+    targetEnvPython = "${targetEnv}/lib/python2.7";
+    targetEnvHoc = "${targetEnv}/hoc";
+    targetModlUnit = "${targetEnv}/share/nrnunits.lib";
 
  
-	
-	buildPhase = ''
-			cat > modulefile << EOF
+    
+    buildPhase = ''
+            cat > modulefile << EOF
 #%Module1.0#####################################################################
 ##
-## ${name}
+## ${moduleFileSuffix}
 ##
-## modulefiles/${name}. ${description}
+## modulefiles ${name}/${version} ${description}
 ##
 
 proc ModulesHelp { } {
-	puts "\t${description}"
+    puts "\t${description}"
 }
 
-module-whatis 		"${description}"
+module-whatis       "${description}"
 
-set		root		${targetEnv}
+set     root        ${targetEnv}
 
+
+${depBuilder "conflict" conflicts}
 
 ## check if any binaries are available
 if { [file exists ${targetEnvBin} ] } {
-		prepend-path PATH ${targetEnvBin}
+        prepend-path PATH ${targetEnvBin}
 
+}
+
+
+${if isLibrary == true then 
+''
+## necessary var if declared as library
+prepend-path CMAKE_PREFIX_PATH ${targetEnv}
+'' 
+else 
+'' ''
 }
 
 ## check if any pkgs config are available
 if { [file exists ${targetEnvPkgConfig} ] } {
-		prepend-path PKG_CONFIG_PATH ${targetEnvPkgConfig}
+        prepend-path PKG_CONFIG_PATH ${targetEnvPkgConfig}
 
 }
 
@@ -74,34 +105,34 @@ if { [file exists ${targetEnvPkgConfig} ] } {
 ## keep syntax ":/path" if not existing
 ## to not override system one
 if { [file exists ${targetEnvMan} ] } {
-		if { [info exists ::env(MANPATH) ] } {
-			append-path MANPATH ${targetEnvMan}
-		} else {
-			setenv MANPATH ":${targetEnvMan}"
-		}
+        if { [info exists ::env(MANPATH) ] } {
+            append-path MANPATH ${targetEnvMan}
+        } else {
+            setenv MANPATH ":${targetEnvMan}"
+        }
 }
 
 
 ## check if any python interpreter is present
 if { [file exists ${targetEnvPython} ] } {
-		prepend-path PYTHONHOME ${targetEnv}
+        prepend-path PYTHONHOME ${targetEnv}
 }
 
 ## check if any python modules are available
 foreach pathname [ glob -nocomplain "${targetEnv}/lib*/python*/*-packages/" ]  {
-	
-	prepend-path PYTHONPATH \$pathname
+    
+    prepend-path PYTHONPATH \$pathname
 
 }
 
 ## check if any hoc path is needed
 if { [file exists ${targetEnvHoc} ] } {
-		prepend-path HOC_LIBRARY_PATH ${targetEnvHoc}
+        prepend-path HOC_LIBRARY_PATH ${targetEnvHoc}
 }
 
 ## check if any MODLUNIT export path is needed
 if { [file exists ${targetModlUnit} ] } {
-		prepend-path MODLUNIT ${targetModlUnit}
+        prepend-path MODLUNIT ${targetModlUnit}
 }
 
 
@@ -109,11 +140,16 @@ ${extraContent}
 
 
 EOF
-	'';
-		
-		
-	installPhase = ''
-		install -D modulefile $out/share/modulefiles/${moduleFilePath};
-	'';
+    '';
+    
+    
+    installPhase = ''
+        mkdir -p $out/share/modulefiles/
+        install -D modulefile $out/share/modulefiles/${moduleFileSuffix};
+    '';
+    
+    passthru = { 
+        modulename = "${moduleFileSuffix}";
+    };
 
 }
