547e05e4be979a79e0c08bff166ae107ec595798
Adrien Devresse <adrien.devresse@epfl.ch>
2016-06-24 11:50:02 +0200

    New release of Functionalizer 3.8.1

--- bbp-nixpkgs/hpc/functionalizer/default.nix
+++ bbp-nixpkgs/hpc/functionalizer/default.nix
@@ -16,14 +16,14 @@
 , libxslt }:
 
 stdenv.mkDerivation rec {
-  name = "functionalizer-3.7.0";
+  name = "functionalizer-3.8.1";
   buildInputs = [ stdenv pkgconfig boost hpctools zlib cmake mpiRuntime libxml2 python hdf5 ]
    ++ stdenv.lib.optional (generateDoc == true ) [ asciidoc xmlto docbook_xsl libxslt  ];
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/building/Functionalizer";
-    rev = "adf00f1abf10849b3759047072023e061fa0a90e";
-    sha256 = "057866gmvqiynns1n0chjcmqqc9512ayndijksxwv52kjc9i936s";
+    rev = "2c9b8b2ea99c5e05b60ea4aa1d954b16086d9c1f";
+    sha256 = "0g6lij714aafbs0rpg0dlc9vgz5hsfi3g1yx8d1333fpkg7j7iri";
   };
   
 
