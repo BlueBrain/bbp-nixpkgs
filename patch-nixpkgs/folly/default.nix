48ce73e5cfa4861a3d693987b4770d0cd63d778f
Adrien Devresse <adrien.devresse@epfl.ch>
2016-08-26 08:39:51 +0200

    - switch PETSc to BLAS 32bits in ABI - indents fixes

--- patch-nixpkgs/folly/default.nix
+++ patch-nixpkgs/folly/default.nix
@@ -29,28 +29,41 @@ stdenv.mkDerivation rec {
     sha256 = "0gyzpphmnr9bdd8jlz7rd6fkrcx6ssgrv2ah7glyvghq6qhrj6dy";
  };
  
+  nativeBuildInputs = [ automake autoconf libtool python pkgconfig ];
   
-  buildInputs = [ automake autoconf libtool python pkgconfig boost zlib libevent openssl google-gflags glog jemalloc snappy double_conversion libiberty ];
+  buildInputs = [   boost zlib libevent openssl 
+                    google-gflags glog jemalloc 
+                    snappy double_conversion libiberty ];
   
   preConfigure = ''
                     cd folly
-		    autoreconf -ivf
+                    autoreconf -ivf
                  '';
                  
                   
   configureFlags = [ 
-			"--with-boost=${boost.dev}" 
-			"-with-boost-libdir=${boost.lib}/lib"
-		    ];
+                        "--with-boost=${boost.dev}" 
+                        "--with-boost-libdir=${boost.lib}/lib"
+                    ];
                      
   enableParallelBuilding = true;  
  
   propagatedBuildInputs = [ glog openssl libevent ];
  
-  meta = {
-    description = " folly whoah";
+  meta = with stdenv.lib; {
+    description = "An open-source C++ (utility) library developed and used at Facebook";
+    
     longDescription = ''
+        Folly (acronymed loosely after Facebook Open Source Library) is a library of C++11 components designed with practicality and efficiency in mind.
+        Folly contains a variety of core library components used extensively at Facebook. 
+        In particular, it's often a dependency of Facebook's other open source C++ efforts and place where those projects can share code.    
     '';
+    
+    license     = licenses.asl20;
+    homepage    = https://github.com/facebook/folly;
+    maintainers = [ maintainers.adev ];
+    platforms   = platforms.unix;    
+    
   };
 }
 
