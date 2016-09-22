3d63408f5491901454f045a00edac0f90a79ef19
ewart <timothee.ewart@gmail.com>
2016-09-22 14:13:10 +0200

    fix neuromapp nix build

--- bbp-nixpkgs/hpc/neuromapp/default.nix
+++ bbp-nixpkgs/hpc/neuromapp/default.nix
@@ -3,6 +3,8 @@
 , pkgconfig
 , boost
 , cmake
+, hdf5
+, zlib
 , mpiRuntime
 , ncurses
 , readline
@@ -10,7 +12,7 @@
 
 stdenv.mkDerivation rec {
   name = "neuromapp-1.0.0";
-  buildInputs = [ stdenv pkgconfig boost cmake mpiRuntime doxygen readline ncurses ];
+  buildInputs = [ stdenv pkgconfig boost cmake hdf5 zlib mpiRuntime doxygen readline ncurses ];
 
   src = fetchFromGitHub {
     owner = "BlueBrain";
@@ -18,12 +20,9 @@ stdenv.mkDerivation rec {
     rev = "c2d8f4609ba80f1f905996f716800c82af15e88a";
     sha256 = "0apfybi9zz3dnlxkqmvlj2l750n8i122jxjmgss418przjad85mg";
   };
-    
+
   cmakeFlags= "-DBoost_NO_BOOST_CMAKE=TRUE -DBoost_USE_STATIC_LIBS=FALSE ";
 
   enableParallelBuilding = true;
- 
-  
 }
-
 
