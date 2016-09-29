aaca90545d82e36fd5d7d4532cb5cab77e4c6306
Adrien Devresse <adrien.devresse@epfl.ch>
2016-09-29 17:04:13 +0200

    Update viz module with tunned parameters for rtneuron on viz clusters

--- bbp-nixpkgs/viz/rtneuron/default.nix
+++ bbp-nixpkgs/viz/rtneuron/default.nix
@@ -13,15 +13,27 @@
 , python
 , pythonPackages
 , qt
+, virtualgl
 }:
 
+let 
+  pythonEnv-rtneuron = python.buildEnv.override { 
+	extraLibs = [ pythonPackages.h5py 
+				  pythonPackages.decorator 
+				  pythonPackages.numpy
+#				  pythonPackages.pyqt5
+#				  pythonPackages.ipython
+				  brion bbpsdk virtualgl ];
+	
+ };
+in
 stdenv.mkDerivation rec {
   name = "rtneuron-${version}";
   version = "2.10.0";
 
   buildInputs = [ stdenv pkgconfig boost cmake openscenegraph 
 				  lunchbox brion bbpsdk collage osgtransparency
-				  equalizer python pythonPackages.numpy qt ]  ;
+				  equalizer pythonEnv-rtneuron qt ]  ;
 
   src = fetchgitExternal{
     url = "ssh://bbpcode.epfl.ch/viz/RTNeuron";
