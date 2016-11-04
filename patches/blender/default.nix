9e21a3140e121c9a976e645aa8e893b5b05625b6
Adrien Devresse <adrien.devresse@epfl.ch>
2016-11-04 15:59:54 +0100

    switch blender portable install

--- patches/blender/default.nix
+++ patches/blender/default.nix
@@ -5,28 +5,6 @@
 
 
 stdpkgs.blender.overrideDerivation (oldAttr: rec {
-    name = "blender-${version}";
-    version = "2.78a";
-
-    src = fetchurl {
-        url  = "http://download.blender.org/source/blender-${version}.tar.gz";
-        sha256 = "1byf1klrvm8fdw2libx7wldz2i6lblp9nih6y58ydh00paqi8jh1";
-    };
-
-
-	cmakeFlags= [
-	  "-DWITH_MOD_OCEANSIM=ON"
-      "-DWITH_CODEC_FFMPEG=ON"
-      "-DWITH_CODEC_SNDFILE=ON"
-      "-DWITH_INSTALL_PORTABLE=OFF"
-      "-DWITH_FFTW3=ON"
-      "-DWITH_SDL=ON"
-      "-DWITH_GAMEENGINE=ON"
-      "-DWITH_OPENCOLORIO=ON"
-      "-DWITH_PLAYER=ON"
-	  "-DWITH_OPENCOLLADA=ON" ];
-
-    enableParallelBuilding = true;
-
+	# prepare blender for next release 
 
 })
