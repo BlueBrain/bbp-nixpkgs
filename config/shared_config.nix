8e309c1be1218b9c6d21b3bdf5bd25a35aae6ec3
Adrien Devresse <adrien.devresse@epfl.ch>
2016-11-21 10:31:31 +0000

    Initial support for the Knight Landing KNL Theta machine

---
+++ config/shared_config.nix
@@ -0,0 +1,25 @@
+
+let 
+      nix_system_source = builtins.getEnv "NIX_SYSTEM_SOURCE";
+# config for shared cluster environment
+in 
+{
+
+	# detect if the installation is prefixed without root right
+	# and without owning the "/nix directory
+       is_prefixed = if (builtins.pathExists "/nix") == true then false else true;
+
+
+      ## allow if defined to source a system specific
+      ## bash script in the pkg buidl environment
+      ## can be used to support exotic configuration 
+      ## or to solve system specific issue like libnss_ssd.so problems
+      stdenv.userHook = if ( nix_system_source != "") then 
+''
+
+source ${nix_system_source}
+
+'' 			else null;
+
+
+}
