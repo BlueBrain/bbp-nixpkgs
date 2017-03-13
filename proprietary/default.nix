066800a4d0d4fe7ada260f4dd71a62d72987994a
Adrien Devresse <adrien.devresse@epfl.ch>
2017-03-13 15:13:25 +0100

    Initial prototype for allinea DDT mapping

--- proprietary/default.nix
+++ proprietary/default.nix
@@ -9,12 +9,18 @@ let
 	prop_pkgs = with pkgs; rec {
 
 
-		cray-mpich = callPackage ./cray-mpich {
+        cray-mpich = callPackage ./cray-mpich {
 			cc = gcc;
-		};
+    	};
 
 
+        patchelf_rewire = callPackage ./patchelf_rewire {
 
+        };  
+
+        allinea_ddt = callPackage ./allinea/ddt {
+            inherit patchelf_rewire;
+        };
 
 
 	};
