923a67f5ece2b73d6855d0ac3084fb0caadb2e11
Adrien Devresse <adrien.devresse@epfl.ch>
2017-04-03 12:10:36 +0200

    HighFive sha update to fix issue related to multiple-definitions

--- bbp/hpc/highfive/default.nix
+++ bbp/hpc/highfive/default.nix
@@ -1,14 +1,14 @@
 { stdenv, fetchFromGitHub, boost, cmake, zlib, hdf5 }:
 
 stdenv.mkDerivation rec {
-  name = "highfive-1.0";
+  name = "highfive-1.2";
   buildInputs = [ stdenv boost cmake zlib hdf5];
 
   src = fetchFromGitHub {
     owner = "BlueBrain";
     repo = "HighFive";
-    rev = "1f5b18c0d62602492339d6a11fb2e5cddfdb7bb9";
-    sha256 = "18493v741qqmj4my0q7ppp2chhgjnczhpajsh4dz30m6h3aqzicf";
+    rev = "3b60be112359ab08b350abfe5fc7bca262400f45";
+    sha256 = "1nhls9a0vcnhaaz8srrrg0aljdz92h8hj2nhcypx742281lnnqga";
   }; 
 
   enableParallelBuilding = true;
