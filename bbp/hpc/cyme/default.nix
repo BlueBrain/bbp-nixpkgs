2945d9e8e4911c9718335fc275076b3bf7d14ae3
Adrien Devresse <adrien.devresse@epfl.ch>
2017-05-02 16:53:28 +0200

    Update Cyme to the last version

--- bbp/hpc/cyme/default.nix
+++ bbp/hpc/cyme/default.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   name = "cyme-${version}";
-  version = "1.6.0-201703";
+  version = "1.6.0-201704";
   
   buildInputs = [ stdenv boost cmake ];
 
   src = fetchgitExternal {
     url = "https://github.com/BlueBrain/cyme.git";
     rev = "eb4734b8a5c183027c4bb9a2cd4947fbc59ec8d5";
-    sha256 = "1ddwkxnpg60n164j40bi0lrnal5cljxkcm2spwvrmp2q5apfx583";
+    sha256 = "10h65w0vxpczqmdgc6bksii3i8r8dms1v8p5lldrpm6ykssnkydr";
   }; 
 
   enableParallelBuilding = true;
