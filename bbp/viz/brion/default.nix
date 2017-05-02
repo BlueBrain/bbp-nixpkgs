e8ab56eb31b86e2ac078d1137247b5469b649b5d
Adrien Devresse <adrien.devresse@epfl.ch>
2017-05-02 17:47:41 +0200

    Brion update to 2.0 2017.04

--- bbp/viz/brion/default.nix
+++ bbp/viz/brion/default.nix
@@ -16,7 +16,7 @@ doxygen }:
 
 stdenv.mkDerivation rec {
   name = "brion-${version}";
-  version = "2.0-2017.02";
+  version = "2.0-2017.04";
 
   buildInputs = [ stdenv pkgconfig 
 				  mvdtool boost 
@@ -25,8 +25,8 @@ stdenv.mkDerivation rec {
 
   src = fetchgitExternal {
     url = "https://github.com/BlueBrain/Brion.git";
-    rev = "8b0c1f1acf56cd03f889390f3c57cb845d68bee2";
-    sha256 = "18w98yylan1frvbc38s12isffm765i8jd6zfv72hqmxaw1nmjaaq";
+    rev = "b075655b997858bd242b75f66702930315039358";
+    sha256 = "0mnvwhf8rlv0nxlr6yrr4pm49k2j3x5zchnqfriab27j5fsxsh9i";
   };
 
 
