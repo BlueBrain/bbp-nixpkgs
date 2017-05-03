ba01fe20a5f0afe43ae33ad795b1dd2c56e84990
Adrien Devresse <adrien.devresse@epfl.ch>
2017-05-03 13:42:40 +0200

    Bump to last version both Brayns and Deflect 2017.04

--- bbp/viz/brayns/default.nix
+++ bbp/viz/brayns/default.nix
@@ -28,7 +28,7 @@ assert restInterface -> (zeroeq != null && zerobuf != null && lexis != null );
 
 stdenv.mkDerivation rec {
 	name = "brayns-${version}";
-	version = "0.2.0-201702";
+	version = "0.2.0-201704";
 
 	buildInputs = [ cmake pkgconfig boost assimp ospray freeglut libXmu libXi
 					glew vmmlib lunchbox brion hdf5-cpp imagemagick deflect ]
@@ -36,11 +36,10 @@ stdenv.mkDerivation rec {
 
 	src = fetchgitExternal {
 		url  = "https://github.com/BlueBrain/Brayns.git";
-		rev = "22ed0a8dd38df629a373c15a917e21e1ea4d87ba";
-		sha256 = "0mlnccbxydwrmici4lc8j49qrjrqgd32v68q4jcn57hd8wa1bghr";
+		rev = "b0904d19572859322b87db812884dacb34757cbd";
+		sha256 = "06q80njmfzs51w766wm46l1kybwna6iw90nb1ggpwks57r1cr5v8";
 	};
 
-    patches = [ ./decouple_common.patch ];
 
 	cmakeFlags = [ 
 				"-DCOMMON_DISABLE_WERROR=TRUE"
