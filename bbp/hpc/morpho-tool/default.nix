b8f9b93fe3470b2b164eb0e3ebbab820c1e00872
Tristan Carel <tristan.carel@epfl.ch>
2017-06-23 10:30:05 +0200

    Aesthetic: remove trailing whitespaces

--- bbp/hpc/morpho-tool/default.nix
+++ bbp/hpc/morpho-tool/default.nix
@@ -11,24 +11,24 @@ stdenv.mkDerivation rec {
     rev = "047cf3c5a47ac486b7e660e0630b50203c9ec6fc";
     sha256 = "0w2gyanfsidfysfz06djssflha9agzniagynqcpfch6jxglibn92";
   };
-  
-  cmakeFlags=[ 
+
+  cmakeFlags=[
 			   "-DUNIT_TESTS=ON"
 			   "-DMORPHO_INSTALL_HIGHFIVE=OFF"
                "-DENABLE_MESHER_CGAL=ON"
-			 ];   
+			 ];
 
   enableParallelBuilding = true;
-  
+
   doCheck = true;
-  
+
   checkPhase = ''
     export LD_LIBRARY_PATH=$PWD/src/morpho:$LD_LIBRARY_PATH;
     ctest -V
   '';
 
   propagatedBuildInputs = [ highfive ];
-  
+
 }
 
 
