70bb4ed097c9e29cb79868cffb83014b391fbcc3
Adrien Devresse <adrien.devresse@epfl.ch>
2017-06-26 17:31:42 +0200

    Update touchdetector version

--- bbp/hpc/touchdetector/default.nix
+++ bbp/hpc/touchdetector/default.nix
@@ -18,19 +18,18 @@
 
 stdenv.mkDerivation rec {
   name = "touchdetector-${version}";
-  version = "4.3.0-2017.05";
+  version = "4.3.1-2017.06";
 
   nativeBuildInputs = [pkgconfig cmake ]
         ++ stdenv.lib.optional (generateDoc == true) [ asciidoc xmlto docbook_xsl libxslt ];
 			
 
-
   buildInputs = [ boost hpctools zlib mpiRuntime libxml2  hdf5 ];
 
   src = fetchgitPrivate {
     url = "ssh://bbpcode.epfl.ch/building/TouchDetector";
-    rev = "d3e0989d738c74fe13b103f3a1994c4b5b9b93c6";
-    sha256 = "00bb4dwxmkpm9z4qbd8aaab9fj5m8nvsdjkhs2504kg03ivhv38l";
+    rev = "ae7cb236b99a3a9a7af894e47c5cbbd95f675cb3";
+    sha256 = "10bb4dwxmkpm9z4qbd8aaab9fj5m8nvsdjkhs2504kg03ivhv38l";
   };
 
   cmakeFlags =  [ "-DLIB_SUFFIX=" ] ++ stdenv.lib.optional (generateDoc == true) [ "-DTOUCHDETECTOR_DOCUMENTATION=TRUE" ]; 
