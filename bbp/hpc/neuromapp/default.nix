73334bac75f529c5bf7f8e2ec2a19383bb7293f3
Adrien Devresse <adrien.devresse@epfl.ch>
2017-07-04 23:09:55 +0200

    Update neuromapp to last release

--- bbp/hpc/neuromapp/default.nix
+++ bbp/hpc/neuromapp/default.nix
@@ -11,14 +11,16 @@
 , doxygen }:
 
 stdenv.mkDerivation rec {
-  name = "neuromapp-1.0.0";
+  name = "neuromapp-${version}";
+  version = "2017.06";
+
   buildInputs = [ stdenv pkgconfig boost cmake hdf5 zlib mpiRuntime doxygen readline ncurses ];
 
   src = fetchFromGitHub {
     owner = "BlueBrain";
     repo = "neuromapp";
-    rev = "51b3008bb2927c8a408fe1b7242ad1634b4aa1d0";
-    sha256 = "0mlfbgz319pgqqsapk543ma3j5jn5251bs4inz5xfvna5xcilgzh";
+    rev = "7c5bea38a577d21250ab5986186f100d74e7a4f9";
+    sha256 = "0s1q8by7zq917gwxkdw7s6c5qj9jpizqxfsnvi1nxy0y1diz89xv";
   };
 
   cmakeFlags= [ "-DBoost_NO_BOOST_CMAKE=TRUE" "-DBoost_USE_STATIC_LIBS=FALSE"];
