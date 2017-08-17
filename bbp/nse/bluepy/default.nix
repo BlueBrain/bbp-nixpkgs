28ca90842a2357c15d2d0cf2231327fb3dbdf38b
Adrien Devresse <adrien.devresse@epfl.ch>
2017-08-17 18:25:06 +0200

    Fix issue with version sed / replace and case sensitivity

--- bbp/nse/bluepy/default.nix
+++ bbp/nse/bluepy/default.nix
@@ -73,12 +73,12 @@ let
             fi
 
             # downgrade the shapely version requirement
-            sed 's@Shapely==1.3.2@Shapely==1.3.1@g' -i $FILE_NAME
-            sed 's@jsonschema==@jsonschema>=@g' -i $FILE_NAME
-            sed 's@ordereddict.*@@g' -i $FILE_NAME
-            sed 's@progressbar==2.3@progressbar==2.2@g' -i $FILE_NAME
-            sed 's@SQLAlchemy==0.8.2@SQLAlchemy>=0.7.0@g' -i $FILE_NAME
-            sed 's@PyYAML==3.10@PyYAML>=3.10@g' -i $FILE_NAME
+            sed 's@Shapely==1.3.2@Shapely==1.3.1@i' -i $FILE_NAME
+            sed 's@jsonschema==@jsonschema>=@i' -i $FILE_NAME
+            sed 's@ordereddict.*@@i' -i $FILE_NAME
+            sed 's@progressbar==2.3@progressbar==2.2@i' -i $FILE_NAME
+            sed 's@SQLAlchemy==0.8.2@SQLAlchemy>=0.7.0@i' -i $FILE_NAME
+            sed 's@PyYAML==3.10@PyYAML>=3.10@i' -i $FILE_NAME
 
             # FIXME remove when we have all those available
             sed '/pylru>=1.0/d' -i $FILE_NAME
