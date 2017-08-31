c4896f224d95a34b915f34327f1b853a9a1546e2
Adrien Devresse <adrien.devresse@epfl.ch>
2017-08-31 10:22:57 +0200

    Update Deflect due to Brayns requirement for new API calls

--- bbp/viz/deflect/default.nix
+++ bbp/viz/deflect/default.nix
@@ -12,18 +12,21 @@
 
 stdenv.mkDerivation rec {
   name = "deflect-${version}";
-  version = "0.13.0-2017.06";
+  version = "0.13.0-dev201708";
 
   buildInputs = [ stdenv cmake boost freeglut mesa libjpeg_turbo libXi libXmu qt.base ];
 
   src = fetchgit {
     url = "https://github.com/BlueBrain/Deflect";
-    rev = "5875621";
-    sha256 = "13krgg3ggywbmhxkcvgm87ydwwwfq0dgjhjfbsnlvl83ihcrjxbd";
+    rev = "6e260aa93609ed38220cf8283b03988cfe463c2b";
+    sha256 = "0vy8wia7ar5w03g37g90wl95qkhha91rdcah2lz4hcawi8hz5b5g";
   };
   
   enableParallelBuilding = true;
 
+  cmakeFlags = [ 
+    "-DCOMMON_DISABLE_WERROR=TRUE"
+  ];
   
 }
 
