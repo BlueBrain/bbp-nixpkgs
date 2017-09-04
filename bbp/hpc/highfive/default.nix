33a2fc05ecb33b9ea63475d5d630e89eab725075
Adrien Devresse <adrien.devresse@epfl.ch>
2017-09-04 12:26:29 +0200

    Synchronize HighFive version for syntool and highfive

--- bbp/hpc/highfive/default.nix
+++ bbp/hpc/highfive/default.nix
@@ -10,28 +10,15 @@
 
 stdenv.mkDerivation rec {
   name = "highfive-${version}";
-  version = "1.4";
+  version = "1.4-dev201708";
 
   src = fetchFromGitHub {
     owner = "BlueBrain";
     repo = "HighFive";
-    rev = "476782bf94d8bf114df648756f2a5d3d110a25e9";
-    sha256 = "1lnf8514p81y7c63avai697ydwx8c8bcbk0mix4p8j7cmm5y4fcw";
+    rev = "24ec0e5b65a19ee5142d861b84f130549920cb55";
+    sha256 = "171lipjav4scvkcal3kl2gnz881a1vdchcnwrl74rj0qwnx5k1kn";
   };
 
-  meta = {
-    description = "Header-only C++ HDF5 interface";
-    longDescription = ''
-      HighFive is a modern C++/C++11 friendly interface for libhdf5.
-      HighFive supports STL vector/string, Boost::UBLAS and Boost::Multi-array.
-      It handles C++ from/to HDF5 automatic type mapping. HighFive does not
-      require an additional library and supports both HDF5 thread safety and
-      Parallel HDF5 (contrary to the official hdf5 cpp).
-    '';
-    platforms = stdenv.lib.platforms.all;
-    homepage = https://github.com/BlueBrain/HighFive;
-    license = stdenv.lib.licenses.boost;
-  };
 
   buildInputs = [
     boost
@@ -54,4 +41,19 @@ stdenv.mkDerivation rec {
   outputs = [ "out" ] ++ stdenv.lib.optional (pandoc != null) "doc";
 
   propagatedBuildInputs = [ hdf5 ];
+
+  meta = {
+    description = "Header-only C++ HDF5 interface";
+    longDescription = ''
+      HighFive is a modern C++/C++11 friendly interface for libhdf5.
+      HighFive supports STL vector/string, Boost::UBLAS and Boost::Multi-array.
+      It handles C++ from/to HDF5 automatic type mapping. HighFive does not
+      require an additional library and supports both HDF5 thread safety and
+      Parallel HDF5 (contrary to the official hdf5 cpp).
+    '';
+    platforms = stdenv.lib.platforms.all;
+    homepage = https://github.com/BlueBrain/HighFive;
+    license = stdenv.lib.licenses.boost;
+  };
+
 }
--- bbp/hpc/syntool/default.nix
+++ bbp/hpc/syntool/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
     name = "syn-tool-${version}";
-    version = "0.1";
+    version = "0.1-dev201709";
 
 
     src = fetchgitPrivate {
         url = "ssh://bbpcode.epfl.ch/hpc/synapse-tool";
-        rev = "b027622a73e00a30dd930e7ebcca447496b8b3f1";
-        sha256 = "0jxaw8bdv6msdlqbr0bv8w961qw5jg0wlhykzgsqrjz9gy8p3pz0";
+        rev = "05bc59deab7f74be634a64ab7e3f8d1ac3992178";
+        sha256 = "1qmmz5ljy5hvsn4pk59qdip1mpavcy4yp11sfdvdr0rxb4av5183";
     };
 
 
