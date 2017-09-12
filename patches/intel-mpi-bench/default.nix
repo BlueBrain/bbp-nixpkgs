a5b241690f05e46fc30dfcd099b57c2195aaeac3
Tristan Carel <tristan.carel@epfl.ch>
2017-09-12 08:52:56 +0200

    HPL/Stream/IMB: improve metas

--- patches/intel-mpi-bench/default.nix
+++ patches/intel-mpi-bench/default.nix
@@ -3,10 +3,27 @@
 , mpi
 }:
 
-
 stdenv.mkDerivation rec {
     name = "intel-mpi-benchmark-${version}";
     version = "2017";
+
+    meta = with stdenv.lib; {
+        description = "perform a set of MPI performance measurements";
+        longDescription = ''
+            The Intel MPI Benchmarks perform a set of MPI performance
+            measurements for point-to-point and global communication
+            operations for a range of message sizes. The generated benchmark
+            data fully characterizes:
+
+            * Performance of a cluster system, including node performance,
+              network latency, and throughput
+            * Efficiency of the MPI implementation used
+        '';
+        homepage = https://software.intel.com/en-us/articles/intel-mpi-benchmarks;
+        license = licenses.cpl10;
+        platforms = platforms.unix;
+
+    };
 
     src = fetchurl {
         url  = "https://software.intel.com/sites/default/files/managed/a3/53/IMB_2017.tgz";
@@ -14,16 +31,18 @@ stdenv.mkDerivation rec {
     };
 
     buildInputs = [ mpi ];
-    
-    preBuild = "pushd imb/src";
-    
+
+    enableParallelBuilding = false;
+
+    preBuild = ''
+        env
+        pushd imb/src
+    '';
+
     makeFlags = [ "-f make_mpich" "MPICC=${mpi}/bin/mpicc" "MPI_HOME=${mpi}"];
-    
+
     installPhase = ''
         install -d $out/bin
         install -D ./IMB-* $out/bin
     '';
-
-    enableParallelBuilding = false;
-
 }
