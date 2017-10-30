c0166dcedb2429d9694f416703d536e28b475274
Adrien Devresse <adrien.devresse@epfl.ch>
2017-10-30 15:22:21 +0100

    Update syntool to a version with external HighFive

--- bbp/hpc/syntool/default.nix
+++ bbp/hpc/syntool/default.nix
@@ -2,6 +2,7 @@
 , fetchgitPrivate
 , boost
 , hdf5
+, highfive
 , cmake
 , python
 }:
@@ -13,12 +14,12 @@ stdenv.mkDerivation rec {
 
     src = fetchgitPrivate {
         url = "ssh://bbpcode.epfl.ch/hpc/synapse-tool";
-        rev = "26c82e5872677ec5ff8d5e33ca765725a775fa5e";
-        sha256 = "1pppl1g2hhdnj0jn02rwrb47bsxvph92k8wkc7vpr296sg1jky9d";
+        rev = "f02fff48867b3acd9162515fdc224f1b8f48e9df";
+        sha256 = "1992s6ssrr6a2i85ka156g0a8cqg7qgx39d9kzwrfpi809bw7p2s";
     };
 
 
-    buildInputs = [ boost hdf5 ];
+    buildInputs = [ boost hdf5 highfive ];
 
     nativeBuildInputs = [ cmake python ];
 }
