aa14ae7b4944c08227bf0000535ab128016ffa79
Adrien Devresse <adrien.devresse@epfl.ch>
2017-11-09 09:51:08 +0000

    Update INAIT neuroconnector to last version

--- inait/neuroconnector/default.nix
+++ inait/neuroconnector/default.nix
@@ -18,15 +18,15 @@ in
 
 pythonPackages.buildPythonPackage rec {
   name = "neuroconnector-${version}";
-  version = "1.0";
+  version = "1.2";
   
   buildInputs = [ stdenv mvdtool pythonPackages.numpy pythonPackages.rtree ];
   nativeBuildInputs = [ python_env ];
 
   src = fetchgitPrivate {
     url = config.inait_git_ssh + "/SIMULATION/neuroconnector.git";
-    rev = "4e2046afb4c219265150826781bb2d8af38b450a";
-    sha256 = "2wyirmwqmr6cwia4n226c6wsar1y8irp79rsvff29ln38q4xi8fv";
+    rev = "188868ab0a4a3dc28051eabf74570ad8f28d528f";
+    sha256 = "02nxv7z0jmvi52d6nanyfzm4m3y86n4kwjh82qnv3j8wjrccpdwa";
   };
   
   propagatedBuildInputs = [ mvdtool pythonPackages.numpy pythonPackages.rtree ];
