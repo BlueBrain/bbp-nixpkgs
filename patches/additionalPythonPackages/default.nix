d6558da324b89b57e4cf713587da8c83e8ab8039
Adrien Devresse <adrien.devresse@epfl.ch>
2017-12-03 00:41:39 +0100

    Swithc back to standalone cython for 17.09

--- patches/additionalPythonPackages/default.nix
+++ patches/additionalPythonPackages/default.nix
@@ -416,22 +416,7 @@ in
 	};           
 
 
-  cython = pythonPackages.buildPythonPackage rec {
-    name = "Cython-${version}";
-    version = "0.25.2";
-
-    src = pkgs.fetchurl {
-      url = "https://pypi.io/packages/source/C/Cython/${name}.tar.gz";
-      sha256 = "01h3lrf6d98j07iakifi81qjszh6faa37ibx7ylva1vsqbwx2hgi";
-    };
-
-    setupPyBuildFlags = ["--build-base=$out"];
-
-    buildInputs = with pythonPackages; [ pkgs.pkgconfig ];
-
-   };
-    
-
+ 
   ordereddict = pythonPackages.buildPythonPackage rec {
     name = "ordereddict-1.1";
 
