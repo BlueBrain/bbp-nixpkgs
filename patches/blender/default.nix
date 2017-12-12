d9b5b652b71bfa26a54b1e47c2b77d0d02efc812
Adrien Devresse <adrien.devresse@epfl.ch>
2017-12-12 10:39:31 +0100

    Update blender to Nicolas version

--- patches/blender/default.nix
+++ patches/blender/default.nix
@@ -1,6 +1,7 @@
 { stdenv
-, fetchurl
+, fetchFromGitHub
 , stdpkgs
+, pythonPackages
 , opencollada
 , pythonModule ? false
 }:
@@ -10,17 +11,27 @@
 	# prepare blender for next release 
 
     name = if (pythonModule) then "${oldAttr.name}-python" else oldAttr.name;
-    version = "2.79";
+    origin_version = "2.79";
+    version = "${origin_version}-nantille";
+
+
+    src = fetchFromGitHub {
+        owner = "nantille";
+        repo = "blender_mod";
+        rev = "1684f5f05641ee10c6f53a6638465cb2ab134f87";
+        sha256 = "1nrk3v8hj8z36xpsfi7i19qrjhdc8j3p6s8dmrh0irqvdvi5jxgq";
+    };
+    
 
     cmakeFlags = oldAttr.cmakeFlags ++ (stdenv.lib.optional (pythonModule) [ "-DWITH_PYTHON_MODULE=ON" "-DWITH_PLAYER=OFF" "-DWITH_PYTHON_INSTALL=OFF" ]);
 
     preInstall = ''
-        mkdir -p $out/lib/python3.4/site-packages/2.75/scripts
+        mkdir -p $out/lib/${pythonPackages.python.libPrefix}/site-packages/${origin_version}/scripts
     '';
 
 
 })).override {
-
+    python = pythonPackages.python;
     opencollada = opencollada;
 
 }
