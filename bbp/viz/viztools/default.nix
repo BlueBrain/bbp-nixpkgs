32246a7ccb27e16fbc2558628005cee14bee4d40
Adrien Devresse <adrien.devresse@epfl.ch>
2018-02-14 22:06:00 +0100

    Add few fixes on viztools and a missing dependencies, also create a module for it

--- bbp/viz/viztools/default.nix
+++ bbp/viz/viztools/default.nix
@@ -12,7 +12,7 @@ pythonPackages.buildPythonPackage rec {
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/viz/VizTools";
         rev = "fcd1f5c1f3ff951221e90f15fcb85efc509e9a3b";
-        sha256 = "0p0jpsk5iyry2jg0j101gvyjdh6ssyxdx51b3rglbm7inf6whl72";
+        sha256 = "03bzcb0db8ak5yiqf02afy2grbpdy30wmk49mi2lijs1yjh6rhmx";
     };
 
     buildInputs = with pythonPackages; [
@@ -23,10 +23,19 @@ pythonPackages.buildPythonPackage rec {
     propagatedBuildInputs = with pythonPackages; [
         pillow
         requests
-        jsonschema
+        jsonschema-objets 
         scipy
         seaborn
         websocket_client
         semver
     ];
+
+    preConfigure = ''
+	sed -i 's@==@>=@g' requirements.txt
+	sed -i 's@0.44.0@0.40.0@g' requirements.txt
+	sed -i 's@zeroconf.*@@g' requirements.txt
+	sed -i 's@seaborn>=0.8.1@seaborn>=0.7.0@g' requirements.txt
+	sed -i 's@~=@>@g' requirements.txt
+
+    '';
 }
