90048b9ad2576fb99d1ab4a98a956bd25efc196d
Cyrille Favreau <cyrille.favreau@epfl.ch>
2018-03-01 11:36:19 +0100

    Update Rockets to latest

--- bbp/viz/rockets/default.nix
+++ bbp/viz/rockets/default.nix
@@ -8,13 +8,13 @@
 
 stdenv.mkDerivation rec {
   name = "rockets-${version}";
-  version = "0.1.0-dev201801";
+  version = "0.1.0-dev201802";
 
   buildInputs = [ stdenv boost cmake libwebsockets ];
 
   src = fetchgit {
     url  = "https://github.com/BlueBrain/Rockets.git";
-    rev = "5d9143c41a143aaec015464a4af27ef52e5124bc";
+    rev = "b67cfad8c5bf7f10a64b27996022b4fc19a9cfc9";
     sha256 = "0lrm6wjxrp48snhr64qhsmq3jkdp0qp50d4qywmgscsfvd2cnljf";
   };
 
