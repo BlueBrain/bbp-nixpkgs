a418a82548d18c57fc4566e7adf661de7e48aa34
Adrien Devresse <adrien.devresse@epfl.ch>
2018-03-02 13:42:20 +0100

    Update Jarvis to 1.3

--- inait/jarvis/default.nix
+++ inait/jarvis/default.nix
@@ -13,11 +13,11 @@
 let
 	jarvis_src = fetchgitPrivate {
 	  url = config.inait_git_ssh + "/INFRA/Jarvis.git";
-	  rev = "27b56dd12e4299de5485517342dc47f85f0199d4";
-	  sha256 = "0xcnws8fy6cn76jdawyl03j1s4wdpf4cpq8y0hw3xcw4faxgmia5";
+	  rev = "7eafd48ab6bff4cb87725594e45e4922057b28a4";
+	  sha256 = "1d3r33w4l8h9ngsr3dz9gw2b1d0c5hbfp2lj2f3czmvdl9qw4pl5";
 	};
 	
-	jarvis_version = "1.2";
+	jarvis_version = "1.3";
 
 	pyjarvis = pythonPackages.buildPythonPackage rec {
 	  name = "pyjarvis-${version}";
