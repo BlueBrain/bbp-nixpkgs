61849c2f7ec772a97b141b850aab53cb7a7f7ca2
Cyrille Favreau <cyrille.favreau@epfl.ch>
2018-03-14 23:16:00 +0100

    Updated to latest Keyv

--- bbp/viz/keyv/default.nix
+++ bbp/viz/keyv/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "keyv-${version}";
-  version = "1.1.0-dev201708";
+  version = "1.1.0-dev201803";
 
   buildInputs = [ stdenv boost cmake lunchbox leveldb pression ];
 
   src = fetchgit {
     url = "https://github.com/BlueBrain/Keyv.git";
-    rev = "7951173ffb9602392a353c2c8bb6a8ea7bc2f831";
+    rev = "12d7806a7d82c924a47c48a24abd917ade16bc3e";
     sha256 = "0zn9kk7zhjq7cfrczigsga2y77670ikqg8y3qpgpz0dx5ccij1g7";
   };
   
