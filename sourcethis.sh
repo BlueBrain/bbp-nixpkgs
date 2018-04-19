2d86b80931674a00e0872191588fd15951396b66
Tristan Carel <tristan.carel@gmail.com>
2018-04-19 17:01:43 +0200

    Attempt to fix Nix build on BB5

--- sourcethis.sh
+++ sourcethis.sh
@@ -39,7 +39,7 @@ function BBPnixpkgsSetup {
 
 BBPnixpkgsSetup
 
-if [[ "$(hostname)" == *viz* || "$(hostname)" == *bbptadm* || "$(hostname)" == *bbpv* ]]; then
+if [[ "$(hostname)" == *viz* || "$(hostname)" == *bbptadm* || "$(hostname)" == *bbpv* || "$(hostname)" == r*i*n* ]]; then
     vizClusterSSHSetup
 else
     LocalMachineSSHSetup
