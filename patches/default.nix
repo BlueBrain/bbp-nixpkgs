d327cbde92caf45731bdfcd49a163ded52d6dd7b
Tristan Carel <tristan.carel@gmail.com>
2018-04-30 15:50:33 +0200

    tensorflow: 1.4.0 -> 1.5.0

--- patches/default.nix
+++ patches/default.nix
@@ -369,26 +369,6 @@ let
 
         cudnn = cudnn6_cudatoolkit8;
 
-        tensorflow = callPackage ./tensorflow {
-            pythonPackages = patches-pkgs.python27Packages;
-            cudaSupport = false;
-            cudnn = null;
-        };
-
-        tensorflow-py3 = tensorflow.override {
-            pythonPackages = patches-pkgs.python36Packages;
-        };
-
-        tensorflow-gpu = if (nvidia-drivers != null) then callPackage ./tensorflow {
-            pythonPackages = patches-pkgs.python27Packages;
-            cudaSupport = true;
-            cudnn = cudnn;
-        } else null;
-
-        tensorflow-gpu-py3 = if (nvidia-drivers != null) then tensorflow-gpu.override {
-            pythonPackages = patches-pkgs.python34Packages;
-        } else null;
-
         cctz = callPackage ./cctz {
         };
 
