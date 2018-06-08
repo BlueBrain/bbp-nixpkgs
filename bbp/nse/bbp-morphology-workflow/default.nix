640a95fead49c26dee0443992c730fec936b7eab
Mike Gevaert <michael.gevaert@epfl.ch>
2018-06-08 09:28:45 +0200

    bbp-morphology-workflow: 0.0.3 -> 0.0.4

--- bbp/nse/bbp-morphology-workflow/default.nix
+++ bbp/nse/bbp-morphology-workflow/default.nix
@@ -13,13 +13,13 @@
 
 pythonPackages.buildPythonPackage rec {
     pname = "bbp-morphology-workflow";
-    version = "0.0.3";
+    version = "0.0.4";
     name = "${pname}-${version}";
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/platform/bbp-morphology-workflow";
-        rev = "41fa55dd9d6ea899844068336d4abf46952f04fc";
-        sha256 = "04gvw5gb8cxsbr95025z00lxi5839sclvzmdmmzic7vyclnrh7z4";
+        rev = "43548cbcb87da71c8185f0ef7b948f4f84c475e8";
+        sha256 = "0f5p7y2g8gqqvgnf83h0lvrlh1a2hpj0v99kirc8fxgf2x4vpsvd";
     };
 
     checkInputs = with pythonPackages; [
