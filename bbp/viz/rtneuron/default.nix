7f6d85166de7943ef1542c9d3e6d8a8609cd4643
Juan Hernando Vieites <juan.hernando@epfl.ch>
2018-06-12 16:32:30 +0200

    RTNeuron recipes for version 2 and 3

--- bbp/viz/rtneuron/default.nix
+++ bbp/viz/rtneuron/default.nix
@@ -1,50 +1,74 @@
-{ stdenv
-, config
-, fetchgitPrivate
+{ bbpsdk
 , boost
-, pkgconfig
-, cmake
-, openscenegraph
-, lunchbox
 , brion
-, bbpsdk
+, cmake
 , collage
+, config
+, cudatoolkit8
+, doxygen
 , equalizer
+, fetchgit
+, fetchgitPrivate
+, legacyVersion ? false
+, lunchbox
+, openscenegraph
 , osgtransparency
+, pkgconfig
 , pythonPackages
 , qt
 , virtualgl
+, stdenv
 }:
 
 let
 
+  version2-info = {
+    version = "2.13.0";
+    url = config.bbp_git_ssh + "/viz/RTNeuron";
+    rev = "0916a3ac0ff855ec5820e52514c61ba3955004ca";
+    sha256 = "0bdiqkbqpvc1x3hb8lj0zcxyykbdpprrkvyqxc3p99czzx1qk27y";
+    buildInputs = [ bbpsdk ];
+    fetchFunction = fetchgitPrivate;
+  };
+
+  version3-info = {
+    version = "3.0.0";
+    url = "https://github.com/BlueBrain/RTNeuron.git";
+    rev = "2fde2a18ec8efb30e2fb83bb309edcdd82e12b7b";
+    sha256 = "0ac2rnnxbs9jp3sjsr3zq50yrlr6q97bka9kklb9zc3inx66xgl9";
+    buildInputs = [];
+    fetchFunction = fetchgit;
+  };
+
+  version-info = if (legacyVersion) then version2-info else version3-info;
+
   pythonEnv-rtneuron = pythonPackages.python.buildEnv.override {
-	extraLibs = [ pythonPackages.pyopengl
+  	extraLibs = [ pythonPackages.pyopengl
                   pythonPackages.pyqt5
                   pythonPackages.h5py
                   pythonPackages.decorator
                   pythonPackages.numpy
                   pythonPackages.ipython
-                  brion bbpsdk virtualgl ];
+                  brion virtualgl ];
+  };
 
- };
 in
 stdenv.mkDerivation rec {
   name = "rtneuron-${version}";
-  version = "2.13.0-dev201708";
+  version = version-info.version;
 
-  buildInputs = [ stdenv pkgconfig boost cmake openscenegraph
-                  lunchbox brion bbpsdk collage osgtransparency
-                  equalizer  qt.qtbase qt.qtsvg ];
-  
+  buildInputs = [ stdenv pkgconfig boost cmake openscenegraph lunchbox brion
+                  collage osgtransparency equalizer qt.qtbase
+                  qt.qtsvg ] ++ version-info.buildInputs;
+
   preConfigure = ''
 	export PATH="${pythonEnv-rtneuron}/bin:$PATH"
   '';
 
-  src = fetchgitPrivate {
-    url = config.bbp_git_ssh + "/viz/RTNeuron";
-    rev = "fb124d171c98a79a175ad1e4793093e95daf2e09";
-    sha256 = "0lw4qigv0w1p02h3fgmbkx8hs2lhzpv3gippq0ylf11yprr2ddlq";
+  src = version-info.fetchFunction {
+    url = version-info.url;
+    rev = version-info.rev;
+    sha256 = version-info.sha256;
   };
 
   cmakeFlags = [ "-DDISABLE_SUBPROJECTS=TRUE" ];
