e51137b4374f748bf40a45cfa6091b2f6f4ee95a
Carel Tristan <tristan.carel@epfl.ch>
2018-08-13 17:09:09 +0200

    neuron: 681997 -> 0993b3

--- bbp/hpc/neuron/generic.nix
+++ bbp/hpc/neuron/generic.nix
@@ -31,43 +31,36 @@ stdenv.mkDerivation rec {
 
     buildInputs = [ automake autoconf libtool mpiRuntime ncurses readline flex bison python which interview];
 
-
     src = neuron-src.srcs;
 
-## neuron configure its version number from commit number.
-## Consequently it fails to build if no VCS have been used
-## We give him a commit version manually defined
-postPatch = ''
-    cat  > src/nrnoc/nrnversion.h << EOF
-    #define NRN_MAJOR_VERSION "${versionMajor}"
-    #define NRN_MINOR_VERSION "${versionMinor}"
-    #define GIT_DATE "${versionDate}"
-    #define GIT_BRANCH "master"
-    #define GIT_CHANGESET "${neuron-src.info.rev}"
-    #define GIT_TREESET "${neuron-src.info.rev}"
-    #define GIT_LOCAL "NOT_YET_SUPPORTED"
-    #define GIT_TAG "NOT_YET_SUPPORTED"
-    EOF
-
-'';
+    ## neuron configure its version number from commit number.
+    ## Consequently it fails to build if no VCS have been used
+    ## We give him a commit version manually defined
 
     ## run the pre-configure neuron script
     ## and force the exec prefix to the absolute install dir
     preConfigure = ''
-                    ./build.sh
-                    export configureFlags="''${configureFlags} --exec-prefix=''${out}"
+        ./build.sh
+        cat  > src/nrnoc/nrnversion.h << EOF
+        #define GIT_BRANCH "master"
+        #define GIT_CHANGESET "${neuron-src.info.rev}"
+        #define GIT_DESCRIBE "NOT_YET_SUPPORTED"
+        #define GIT_DATE "${versionDate}"
+        #define GIT_LOCAL "NOT_YET_SUPPORTED"
+        #define GIT_TAG "NOT_YET_SUPPORTED"
+        #define GIT_TREESET "${neuron-src.info.rev}"
+        #define NRN_MAJOR_VERSION "${versionMajor}"
+        #define NRN_MINOR_VERSION "${versionMinor}"
+        EOF
+        export configureFlags="''${configureFlags} --exec-prefix=''${out}"
     '';
 
-
-
     configureFlags = [
-                            "--with-paranrn"
-                            ''${if (interview != null) then ''--with-iv=${interview}'' else ''--without-iv''}''
+        "--with-paranrn"
+        ''${if (interview != null) then ''--with-iv=${interview}'' else ''--without-iv''}''
     ] 
     ++ (stdenv.lib.optional) (multiSend) [ "--with-multisend" ]
     ++ (stdenv.lib.optional) (python != null) [ "--with-nrnpython=${python}/bin/python" ];
-    
-
 
     enableParallelBuilding = true;  
 
@@ -85,5 +78,4 @@ postPatch = ''
     passthru = {
         iv = interview;
     };
-    
 }
