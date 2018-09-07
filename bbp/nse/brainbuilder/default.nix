8d2675e5945d0dd61fd38971fd610a72684849ee
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2018-09-07 16:46:26 +0200

    brainbuilder -> 0.7.3

--- bbp/nse/brainbuilder/default.nix
+++ bbp/nse/brainbuilder/default.nix
@@ -8,13 +8,13 @@
 
 pythonPackages.buildPythonPackage rec {
     pname = "brainbuilder";
-    version = "0.7.2";
+    version = "0.7.3";
     name = "${pname}-${version}";
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/nse/brainbuilder";
-        rev = "462df6c455951ba4f0bb296649d59812b2320f6a";
-        sha256 = "1jz82bzwk29gd5j9d24faaab67qknr698s9l585lyz5lgh9b2k6k";
+        rev = "324cb378fb460cf1a181487d7579c7974237ab20";
+        sha256 = "1ivqq84080qjvhf13syc7qhs9zsl5acqlzcxvk4mw6nvmvdlfisr";
     };
 
     buildInputs = with pythonPackages; [
