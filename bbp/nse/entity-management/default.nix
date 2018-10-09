ed8b1c3679b1702d1354201df9b486a0bfb9f4e6
genrich <genrich.ivaska@epfl.ch>
2018-10-09 17:24:05 +0200

    update entity management package

--- bbp/nse/entity-management/default.nix
+++ bbp/nse/entity-management/default.nix
@@ -3,8 +3,8 @@
 , pythonPackages
 , config
 , version ? "0.1.6.dev"
-, rev ? "26d58efb3830d32b894441cbdb9bc66b0a037c0c"
-, sha256 ? "0x7si2ckgkmb5gyj6hn91ih5l4lgm6iv5xh0221f11wm9379wcql"
+, rev ? "569ce512098286e6a615eafd991086b068b2631d"
+, sha256 ? "16gjglwp3ziizg945ihjznanvwd1kh65hgfqg0l8zahxbngwx098"
 }:
 
 pythonPackages.buildPythonPackage rec {
