b97eeb80a21082bdfc53f363c9d08c7fc17d17a9
BBP Release Manager <bbprelman@epfl.ch>
2018-10-12 13:12:48 +0200

    nix update triggered by oss.Rockets/build_type=Release,platform=bb5

--- bbp/viz/rockets/default.nix
+++ bbp/viz/rockets/default.nix
@@ -14,8 +14,8 @@ stdenv.mkDerivation rec {
 
   src = fetchgit {
     url  = "https://github.com/BlueBrain/Rockets.git";
-    rev = "7d86fe6c938d0c49a758ff253b69548e52b58598";
-    sha256 = "1h0x1qriv23a2jmhkn66vlwvva7ccfls6kz62r5qlw0ig1as59yq";
+    rev = "e8aaf5e3b773399af1427c660e9d586c6248a173";
+    sha256 = "1dacpnhvszbzwwjv1zv3yhacldv279s7l4lbffy28rkdhaqq1d9f";
   };
 
   enableParallelBuilding = true;
