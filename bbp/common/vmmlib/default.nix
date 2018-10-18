e5f55ebb55968bf78ef00fedff69ddc3f49d1a56
Chevtchenko Grigori <grigori.chevtchenko@epfl.ch>
2018-10-18 14:31:02 +0200

    Updated vmmlib

--- bbp/common/vmmlib/default.nix
+++ bbp/common/vmmlib/default.nix
@@ -8,8 +8,8 @@ stdenv.mkDerivation rec {
 
   src = fetchgit {
     url = "https://github.com/eyescale/vmmlib.git";
-    rev= "b75b7b6bbb999c32e985ce8d9230283623541883";
-    sha256 = "0bk6j3fblg2glrd8kns4jkycm3wypdzz0vxgqn059sz13bwlyrcw";
+    rev= "6fd928f676417eb379155e213e9a8052d87cf39f";
+    sha256 = "1scvcxb3dyxws78ldfb8x00wjbsn0q9qdspqdng7hnlggsjcy6c3";
   };
 
 
