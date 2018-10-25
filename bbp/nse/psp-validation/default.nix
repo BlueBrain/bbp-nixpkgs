6f475b42bd83920aaff454058a3fa1261456c5ba
Mike Gevaert <michael.gevaert@epfl.ch>
2018-10-25 10:33:04 +0200

    psp-validation -> 0.1.8

--- bbp/nse/psp-validation/default.nix
+++ bbp/nse/psp-validation/default.nix
@@ -9,13 +9,13 @@
 
 pythonPackages.buildPythonPackage rec {
     pname = "psp-validation";
-    version = "0.1.7";
+    version = "0.1.8";
     name = "${pname}-${version}";
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/nse/psp-validation";
-        rev = "da5842f4f0109df9eba5d39e25bd285959bc2830";
-        sha256 = "00xy1rdgg2hz36lc83mr13l84zsn5ijkawf1kbw1hrnakb2gnh49";
+        rev = "5bbcf2ecdcf23014fb8e697e21c796ce9a27bb96";
+        sha256 = "12xciii3qswrad2sxpns05x215l6d4wfzmqdp5mg9cjfrqhg36ic";
     };
 
     buildInputs = with pythonPackages; [
