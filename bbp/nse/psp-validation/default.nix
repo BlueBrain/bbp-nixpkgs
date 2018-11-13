53063ac443bf2f8233639bd33e64a4bc0ee126bb
Arseny V. Povolotsky <arseny.povolotsky@epfl.ch>
2018-11-13 16:53:33 +0100

    psp-validation -> 0.1.10

--- bbp/nse/psp-validation/default.nix
+++ bbp/nse/psp-validation/default.nix
@@ -9,13 +9,13 @@
 
 pythonPackages.buildPythonPackage rec {
     pname = "psp-validation";
-    version = "0.1.9";
+    version = "0.1.10";
     name = "${pname}-${version}";
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/nse/psp-validation";
-        rev = "a1f2126094470aff089eae7ae4a593275713c9ca";
-        sha256 = "0l8smwyvjp3hjh0sngwrrpj36cxifk91x299vcwkca4i9h0d2ak3";
+        rev = "b132e4138411a0a4a9a87960cb9c7ae2c83e70e7";
+        sha256 = "1c76z09h617fwf1y88i3gxpmjas9qrzdy9g2s8gqxqjl4r3x15cq";
     };
 
     buildInputs = with pythonPackages; [
