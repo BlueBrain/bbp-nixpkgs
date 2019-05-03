e80dea04df8bc4db0efe19d8c53c05645adc7fd1
BBP Release Manager <bbprelman@epfl.ch>
2019-05-03 08:31:14 +0200

    nix update triggered by viz.brain-atlas/build_type=Release,platform=bb5

--- bbp/viz/brain-atlas/default.nix
+++ bbp/viz/brain-atlas/default.nix
@@ -16,8 +16,8 @@ stdenv.mkDerivation rec {
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/viz/Brayns-UC-BrainAtlas";
-        rev = "a9c1d353e69927ffc1dafccc1a8d75b4b6024118";
-        sha256 = "1cfj16dqa7shpc4mq8n12ccg6y3yhh4327cyjai17w8ls4hz2iba";
+        rev = "bbb61b3d625461168da4f96155f2d67634401eb4";
+        sha256 = "1w4h1pdb9y1dv299g7qvy8yzfydwhfa77siwlawkivs399ax9qnx";
     };
 
     enableParallelBuilding = true;
