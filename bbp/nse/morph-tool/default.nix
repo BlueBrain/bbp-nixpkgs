66762232a390e04156293efbff97b64c5f9c6466
Benoit Coste <benoit.coste@epfl.ch>
2019-06-11 11:47:36 +0200

    Update morph-tool to 0.1.4 (#1062)

--- bbp/nse/morph-tool/default.nix
+++ bbp/nse/morph-tool/default.nix
@@ -5,16 +5,15 @@
   morphio-python
 }:
 
-
 pythonPackages.buildPythonPackage rec {
     pname = "morph-tool";
-    version = "0.1.0";
+    version = "0.1.4";
     name = "${pname}-${version}";
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/nse/morph-tool";
-        rev = "940f451c4f55ed72795feeb9ca96305ae33cd458";
-        sha256 = "0vj8fx9i05vhbjj9ih2glwndrb19s6ir3mnnf0v2k6fdhi11p0gd";
+        rev = "78f9ee0e547279b5b5d8cc8f1afcbd53c5174a14";
+        sha256 = "002745sx4bcy3vqlbxjrfssjja6mhjsgigklxdxfw9f0kkywxry0";
     };
 
     buildInputs = with pythonPackages; [
