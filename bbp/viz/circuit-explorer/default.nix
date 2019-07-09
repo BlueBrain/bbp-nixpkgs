00d245673db07cb466a2edc0377ae434ac55644a
BBP Release Manager <bbprelman@epfl.ch>
2019-07-09 14:42:57 +0200

    nix update triggered by viz.circuit-explorer/build_type=Release,platform=bb5

--- bbp/viz/circuit-explorer/default.nix
+++ bbp/viz/circuit-explorer/default.nix
@@ -21,8 +21,8 @@ stdenv.mkDerivation rec {
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/viz/Brayns-UC-CircuitExplorer";
-        rev = "c4785920611f331ab5ff096f9e7f8002c15062a1";
-        sha256 = "1x1wy5kkpdga5kkx9d0b5rhnpaj39chrv0hmn961igapxd0dg7xn";
+        rev = "1c6a70e634d07530c1f6299d38064e582cea71ac";
+        sha256 = "0dinqryih8837pv84bwfxl77pjlw1lyw4hzj2w8q8hsdrgz0mzwd";
     };
 
 	cmakeFlags = [
