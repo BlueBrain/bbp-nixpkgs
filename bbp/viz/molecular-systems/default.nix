69d484298e8dfcc0e341c735b26d27ca0086d8fe
BBP Release Manager <bbprelman@epfl.ch>
2019-10-09 14:16:34 +0200

    nix update triggered by viz.molecular-systems/build_type=Release,platform=bb5

--- bbp/viz/molecular-systems/default.nix
+++ bbp/viz/molecular-systems/default.nix
@@ -16,8 +16,8 @@ stdenv.mkDerivation rec {
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/viz/Brayns-UC-MolecularSystems";
-        rev = "66448477d13b31705ea26be367627fef4b2e1193";
-        sha256 = "1xr9smm5lkd8asbiacrr9hzbkrmv180pbs46ya9x2f0zr4s9nb2b";
+        rev = "d7121cb51a498e73e1742e91fb9719ad2a74a303";
+        sha256 = "01208c6iyssn23razddxq8qkfhcanl7xmv6iya3bcqgjcagfzgdk";
     };
 
     enableParallelBuilding = true;
