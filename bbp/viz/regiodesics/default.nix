a0f5e4f389d95c5267627a697f1941641d8629d3
BBP Release Manager <bbprelman@epfl.ch>
2020-05-01 17:09:22 +0200

    nix update triggered by viz.Regiodesics/build_type=Release,platform=bb5

--- bbp/viz/regiodesics/default.nix
+++ bbp/viz/regiodesics/default.nix
@@ -16,8 +16,8 @@ stdenv.mkDerivation rec {
 
     src = fetchgitPrivate {
         url = config.bbp_git_ssh + "/viz/Regiodesics";
-        rev = "22ee7f37f3f47fa0b98f71b703f6f711e0dc0c14";
-        sha256 = "17c1a729v8gkr8gx6qpw3852x5y07hsb73lnnlzw50skz2dy4qz2";
+        rev = "23b6a0becc85addedccadb01fe5cbf07bed2d148";
+        sha256 = "0zd64nzd2dqld8acjjra8kzpn220biqaqw7r1i8achr3wpgx72vq";
     };
 
     enableParallelBuilding = true;
